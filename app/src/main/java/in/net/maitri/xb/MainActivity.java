007b94384fa4c03aa737705b717ff622813e9c0d
vishnuruban <vishnuruban93@gmail.com>
2017-11-03 11:57:16 +0530

    Initial commit

---
+++ app/src/main/java/in/net/maitri/xb/MainActivity.java
@@ -0,0 +1,13 @@
+package in.net.maitri.xb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
