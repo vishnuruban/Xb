95c6342c763557fc6cb6f20511d3acc84bce0316
Nisith <nisithasadangi@gmail.com>
2017-11-18 15:49:11 +0530

    Settings(Company, Bill, Printer)

--- app/src/main/java/in/net/maitri/xb/itemdetails/AddItemCategory.java
+++ app/src/main/java/in/net/maitri/xb/itemdetails/AddItemCategory.java
@@ -1,6 +1,7 @@
 package in.net.maitri.xb.itemdetails;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
@@ -11,6 +12,7 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -24,6 +26,7 @@ import in.net.maitri.xb.R;
 import in.net.maitri.xb.db.Category;
 import in.net.maitri.xb.db.DbHandler;
 import in.net.maitri.xb.db.Item;
+import in.net.maitri.xb.settings.SettingsActivity;
 import in.net.maitri.xb.util.Permissions;
 
 public class AddItemCategory extends AppCompatActivity {
@@ -40,6 +43,11 @@ public class AddItemCategory extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item_category);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
 
         new Permissions(AddItemCategory.this).checkWriteExternalStoragePermission();
 
@@ -250,5 +258,27 @@ public class AddItemCategory extends AppCompatActivity {
             mNoItem.setVisibility(View.GONE);
         }
         mItemAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_add_item_category, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.settings:
+                startActivity(new Intent(AddItemCategory.this, SettingsActivity.class));
+                break;
+
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/in/net/maitri/xb/settings/CompanySettings.java
@@ -0,0 +1,53 @@
+package in.net.maitri.xb.settings;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+
+import in.net.maitri.xb.R;
+
+public class CompanySettings extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.settings_company);
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        for (int i = 0; i < getPreferenceScreen().getPreferenceCount(); ++i) {
+            Preference preference = getPreferenceScreen().getPreference(i);
+            if (preference instanceof PreferenceCategory) {
+                PreferenceCategory preferenceGroup = (PreferenceCategory) preference;
+                for (int j = 0; j < preferenceGroup.getPreferenceCount(); ++j) {
+                    Preference singlePref = preferenceGroup.getPreference(j);
+                    updatePreference(singlePref, singlePref.getKey());
+                }
+            } else {
+                updatePreference(preference, preference.getKey());
+            }
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        updatePreference(findPreference(key), key);
+    }
+
+    private void updatePreference(Preference preference, String key) {
+        if (preference == null) return;
+        if (preference instanceof EditTextPreference) {
+            EditTextPreference listPreference = (EditTextPreference) preference;
+            listPreference.setSummary(listPreference.getText());
+        }
+
+    }
+}
